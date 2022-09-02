e88cf5c47e65e88fb58792ae9fbc7faa3ac2654f
Alex <sanyasan123@mail.ru>
2022-09-02 13:14:54 +0300

    H_5

--- Home_5/Program.cs
+++ Home_5/Program.cs
@@ -5,3 +5,36 @@
 // 11 16 15 06
 // 10 09 08 07
 
+int [,] quadro = new int[4,4];
+int num = 1;
+int i =0;
+int j =0;
+
+while(num <= 16)  // 4*4    GetLength(0)-1 = 3   GetLength(1)-1 = 3
+{
+    quadro[i, j] = num;
+    num++;
+    if(i <= j + 1 && j + i < 3) j++;                // j == 3
+    
+    else if(i < j && i + j >= 3) i++;                // i == 3
+    
+    else if(i >= j && j + i > 3) j--;               
+    
+    else i--;
+}
+
+void PrintQuadro(int[,] quadro)
+{
+    for(int x = 0; x < 4; x++)
+    {
+        for(int y = 0; y < 4; y++)
+        {
+            Console.Write($"{quadro[x, y]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+// FillQuadro(i, j);
+PrintQuadro(quadro);
